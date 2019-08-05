dc2c67240ed6297b63918991d0d4cb98768284b3
Thy Ton <maithytonn@gmail.com>
2019-08-05 14:48:28 -0700

    finished a* search to count moves and record steps

--- a_search.py
+++ a_search.py
@@ -1,17 +1,40 @@
-from helper import distance
-from heapq import heappush, heappop
-def aStarSearch(start, goals, adjList):
-	return None
-	heap = []
-	heappush(heap, ( sorted([distance(start, goal) for goal in goals][0], start )))
+"""
+A* Search 
+Thy Ton
+"""
 
-	current = start
+from helper import distance, Act
+from heapq import heappush, heappop
+
+def aStarSearch(start, goals, adjList):
+	heap = []
+	fromStart = {}
+	fromStart[start] = 0
+	previousPos = {}
+	heappush(heap, (min([distance(start, goal) for goal in goals]), start) )
 	while heap:
+		current = heappop(heap)[1]
 		if current in goals:
 			# calculate plans
-			return
+			moveCnt = fromStart[current]
+			steps = []
+			pos = current
+			while pos != start:
+				steps.insert(0, pos)
+				pos = previousPos[pos]
+			steps.insert(0, start)
+			return { 'steps' : steps, 'plan': [Act.STEP]*moveCnt}
+		neis = adjList[current]
+		for nei in neis:
+			if nei not in fromStart:
+				previousPos[nei] = current
+				fromStart[nei] = fromStart[current] + 1 
+				priority = fromStart[nei] + min([distance(nei, goal) for goal in goals])
+				heappush(heap, (priority, nei))
+			elif fromStart[current] + 1 < fromStart[nei]:
+			# if there is a shorter path to nei
+				fromStart[nei] = fromStart[current] + 1
+				previousPos[nei] = current
+				priority = fromStart[nei] + min([distance(nei, goal) for goal in goals])
 
-		current = heappop(heap)
-		for nei in adjList[current]:
-			priority = distFromCurrent + distToGoal
-			heappush(heap, (priority, nei))
+
